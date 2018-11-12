acc6a2b571308026893a812584979dda41cf0651
Miguel Garcia Rodriguez <miguel.garciarod@gmail.com>
2018-11-12 20:28:35 +0100

    Configure Django DATABASES from environment variable

--- skatepedia/settings.py
+++ skatepedia/settings.py
@@ -79,7 +79,6 @@ WSGI_APPLICATION = 'skatepedia.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -88,8 +87,7 @@ DATABASES = {
 }
 
 if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(os.environ['DATABASE_URL'])
-
+    DATABASES['default'] = dj_database_url.parse(os.environ['DATABASE_URL'])
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
