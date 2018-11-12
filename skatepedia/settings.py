3b82d77c195ade4f38efa04a43f78975b01fc88f
Miguel Garcia Rodriguez <miguel.garciarod@gmail.com>
2018-11-12 18:29:39 +0100

    Allow heroku test app as host

--- skatepedia/settings.py
+++ skatepedia/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'skatepedia-api.herokuapp.com'
+]
 
 # Application definition
 
